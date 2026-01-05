fb59cf3a330d2bd3f35f3becce787521277690a5
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-05 20:52:34 +0100

    Readd assertion for NaNs

--- trak/traker.py
+++ trak/traker.py
@@ -406,6 +406,7 @@ class TRAKer:
         grads = self.projector.project(grads, model_id=self.saver.current_model_id)
         grads /= self.normalize_factor
 
+        assert not grads.isnan().any()
         self.saver.current_store["grads"][inds] = grads.to(self.dtype).cpu().clone().detach()
 
         loss_grads = self.gradient_computer.compute_loss_grad(batch)
